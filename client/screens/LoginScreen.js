e795e2681a4f1a1c4cbe60dc8446ff7f85652edd
kshiflett88 <kodishiflett@gmail.com>
2021-01-22 11:04:12 -0800

    Added inputs/button/logo in login screen

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,17 +1,114 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import { Formik } from 'formik'
 
 const LoginScreen = () => {
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior="padding"
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <Formik
+          initialValues={{
+            email: "",
+            password: ""
+          }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
+        >
+          {(props) => (
+            <View style={styles.container}>
+              <View style={styles.logo}>
+                <Image source={require('../assets/images/pairpro.png')} style={styles.image} />
+              </View>
+              <View>
+                <TextInput 
+                  style={styles.input} 
+                  placeholder="Email"
+                  placeholderTextColor="#fff"
+                  keyboardType="email-address"
+                />
+                 <TextInput 
+                  style={styles.input} 
+                  placeholder="Password"
+                  placeholderTextColor="#fff"
+                  secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.button} >
+                  <Text style={styles.buttonText} >Login</Text>
+                </TouchableOpacity>
+                <View style={styles.registerContainer}>
+                  <Text style={styles.registerText}>Don't have account?</Text>
+                  <TouchableOpacity >
+                    <Text style={styles.registerButton}>Register</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+        </Formik>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  logo: {
+    alignItems: 'center',
+    marginVertical: 40
+  },
+  image: {
+    width: 300,
+    height: 300
+  },
+  input: {
+    width: 300, 
+    backgroundColor: '#B6BFC4',
+    borderRadius: 25,
+    padding: 16, 
+    fontSize: 16,
+    marginVertical: 10
+  }, 
+  button: {
+    width: 300,
+    backgroundColor: '#738289',
+    borderRadius: 25, 
+    marginVertical: 10,
+    paddingVertical: 13
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff',
+    textAlign: 'center'
+  },
+  registerContainer: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingVertical: 16, 
+    flexDirection: 'row'
+  },
+  registerText: {
+    color: '#738289',
+    fontSize: 16
+  },
+  registerButton: {
+    color: '#738289',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  error: {
+    color: 'red'
+  }
 })
 
 export default LoginScreen;
