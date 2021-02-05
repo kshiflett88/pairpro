bfb86f5fdd2494981e23661a002ee63b014ce184
kshiflett88 <kodishiflett@gmail.com>
2021-02-05 12:27:34 -0800

    Create and update a profile

--- pairproweb/routes/api/profile.js
+++ pairproweb/routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult} = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -22,6 +23,82 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 });
+
+//@route    POST api/profile
+//@desc     Create or Update a user profile
+//@access   Private
+router.post('/', [ auth, [
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills are required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
+
+  // Build profile object
+  const profilefields = {};
+  profilefields.user = req.user.id;
+  if (company) profilefields.company = company;
+  if (website) profilefields.website = website;
+  if (location) profilefields.location = location;
+  if (bio) profilefields.bio = bio;
+  if (status) profilefields.status = status;
+  if (githubusername) profilefields.githubusername = githubusername;
+  if(skills) {
+    // Doesn't matter if there is any amount of spaces
+    profilefields.skills = skills.split(',').map(skill => skill.trim());
+  }
+
+  // Build social object
+  profilefields.social = {};
+  if (youtube) profilefields.social.youtube = youtube;
+  if (twitter) profilefields.social.twitter = twitter;
+  if (facebook) profilefields.social.company = company;
+  if (linkedin) profilefields.social.linkedin = linkedin;
+  if (instagram) profilefields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    if(profile) {
+      // Update
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id }, 
+        { $set: profilefields},
+        { new: true }
+      );
+
+      return res.json(profile);
+    }
+
+    // Create
+    profile = new Profile(profilefields);
+
+    await profile.save();
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+  res.send('Hello')
+})
 
 
 module.exports = router;
