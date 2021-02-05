09e2e78fb0593343c64f90298995f3fa15c52518
kshiflett88 <kodishiflett@gmail.com>
2021-02-05 12:44:49 -0800

    Route for getting profile by user ID

--- pairproweb/routes/api/profile.js
+++ pairproweb/routes/api/profile.js
@@ -111,6 +111,29 @@ router.get('/', async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server error')
   }
+});
+
+//@route    GET api/profile/user/:user_id
+//@desc     Get profile by user ID
+//@access   Publice
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id 
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found'});
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found'});
+    }
+
+    res.status(500).send('Server error')
+  }
 })
 
 
