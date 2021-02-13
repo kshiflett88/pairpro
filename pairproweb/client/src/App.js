1078bb9a3dbc7468d43fc6380b74c3b81c63659c
kshiflett88 <kodishiflett@gmail.com>
2021-02-12 20:35:20 -0800

    Login and register routes/pages

--- pairproweb/client/src/App.js
+++ pairproweb/client/src/App.js
@@ -1,15 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 
+//Everything has a class of container to push everything to middle
+//Landing page doesnt have that cause want it to go all the way to the edge
 
 const App = () => (
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container"> 
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+  </Router>
 )
 
 export default App;
