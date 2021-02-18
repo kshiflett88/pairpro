29b38056e672e2305dc1ba70a2561cffc870d57f
kshiflett88 <kodishiflett@gmail.com>
2021-02-18 12:27:13 -0800

    Create profile form finished

--- pairproweb/client/src/components/profile-form/CreateProfile.js
+++ pairproweb/client/src/components/profile-form/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
-// import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
 
+import { createProfile } from '../../actions/profile';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-const CreateProfile = props => {
+const CreateProfile = ({createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -38,6 +40,11 @@ const CreateProfile = props => {
   } = formData;
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
   return (
     <Fragment>
       <h1 className="large text-primary">
@@ -48,7 +55,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -150,7 +157,9 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired,
 }
 
-export default CreateProfile
+
+{/* export default CreateProfile */}
+export default connect(null, { createProfile })(withRouter(CreateProfile))
