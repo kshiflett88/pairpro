1dcb5fcbfd6caa29610287d9ae053f17f86d4cd3
kshiflett88 <kodishiflett@gmail.com>
2021-02-23 14:22:50 -0800

    Github repos added to profile page

--- pairproweb/client/src/components/profile/Profile.js
+++ pairproweb/client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 
 
@@ -58,6 +59,9 @@ const Profile = ({ match }) => {
                   </Fragment>
                 ) : (<h4>No experience credentials</h4>)}
               </div>
+              {profile.githubusername && (
+                <ProfileGithub username={profile.githubusername} />
+              )}
           </div>
         </Fragment>
       )}
--- pairproweb/routes/api/profile.js
+++ pairproweb/routes/api/profile.js
@@ -320,9 +320,14 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      uri: encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      ),
       method: 'GET',
-      headers: { 'user-agent': 'node.js'}
+      headers: { 
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      }
     };
 
     request(options, (error, response, body) => {
