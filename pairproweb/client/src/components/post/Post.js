2960335c019712dcbc02afdcea9baba6338755c9
kshiflett88 <kodishiflett@gmail.com>
2021-02-25 16:31:40 -0800

    Showing a single post

---
+++ pairproweb/client/src/components/post/Post.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getPost } from '../../actions/post';
+import PostItem from '../posts/PostItem';
+import { Link } from 'react-router-dom';
+
+const Post = ({ match }) => {
+  const dispatch = useDispatch();
+  const {post, loading} = useSelector(state => state.post);
+
+  useEffect(() => {
+    dispatch(getPost(match.params.id))
+  }, [])
+
+  return loading || post === null ? (
+      <Spinner /> 
+    ) : (
+      <Fragment>
+        <Link to="/posts" className="btn">
+          Back To Posts
+        </Link>
+        <PostItem post={post} showActions={false} />
+      </Fragment>
+    )
+}
+
+export default Post
--- pairproweb/client/src/components/posts/PostItem.js
+++ pairproweb/client/src/components/posts/PostItem.js
@@ -4,7 +4,7 @@ import Moment from 'react-moment';
 import { useDispatch, useSelector } from 'react-redux';
 import {addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({ post: {_id, text, name, avatar, user, likes, comments, date}}) => {
+const PostItem = ({ post: {_id, text, name, avatar, user, likes, comments, date}, showActions=true}) => {
 
   const dispatch = useDispatch();
   const auth = useSelector(state => state.auth);
@@ -29,23 +29,27 @@ const PostItem = ({ post: {_id, text, name, avatar, user, likes, comments, date}
              <p className="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button onClick={e => dispatch(addLike(_id))} type="button" className="btn btn-light">
-              <i className="fas fa-thumbs-up"></i>{' '}
-              <span>{likes.length > 0 && (<span>{likes.length}</span>)}</span>
-            </button>
-            <button onClick={e => dispatch(removeLike(_id))} type="button" className="btn btn-light">
-              <i className="fas fa-thumbs-down"></i>
-            </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
-              Discussion {comments.length > 0 && (
-                <span className='comment-count'>{comments.length}</span>
-              )}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-              <button onClick={e => dispatch(deletePost(_id))} type="button" className="btn btn-danger">
-                <i className="fas fa-times"></i>
-              </button>
-            )}
+            {showActions && 
+              <Fragment>
+                <button onClick={e => dispatch(addLike(_id))} type="button" className="btn btn-light">
+                  <i className="fas fa-thumbs-up"></i>{' '}
+                  <span>{likes.length > 0 && (<span>{likes.length}</span>)}</span>
+                </button>
+                <button onClick={e => dispatch(removeLike(_id))} type="button" className="btn btn-light">
+                  <i className="fas fa-thumbs-down"></i>
+                </button>
+                <Link to={`/posts/${_id}`} className="btn btn-primary">
+                  Discussion {comments.length > 0 && (
+                    <span className='comment-count'>{comments.length}</span>
+                  )}
+                </Link>
+                {!auth.loading && user === auth.user._id && (
+                  <button onClick={e => dispatch(deletePost(_id))} type="button" className="btn btn-danger">
+                    <i className="fas fa-times"></i>
+                  </button>
+                )}
+              </Fragment>
+            }
           </div>
         </div>
     </Fragment>
