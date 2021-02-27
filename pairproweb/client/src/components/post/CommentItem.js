166bd9b3e9f37fdb4d45b954aa0b34650a15751b
kshiflett88 <kodishiflett@gmail.com>
2021-02-26 21:46:13 -0800

    Adding and deleting comments to a post

---
+++ pairproweb/client/src/components/post/CommentItem.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
+
+const CommentItem = ({
+  postId,
+  comment: {_id, text, name, avatar, user, date}
+}) => {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+  return (
+    <div className="post bg-white p-1 my-1">
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img
+            className="round-img"
+            src={avatar}
+            alt=""
+          />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p className="my-1">
+          {text}
+        </p>
+          <p className="post-date">
+            <Moment format='YYYY/MM/DD'>{date}</Moment>
+        </p>
+        {!auth.loading && user === auth.user._id && (
+          <button onClick={e => dispatch(deleteComment(postId, _id))} type="button" className="btn btn-danger">
+            <i className="fas fa-times"></i>
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default CommentItem
--- pairproweb/client/src/components/post/Post.js
+++ pairproweb/client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ match }) => {
   const dispatch = useDispatch();
@@ -23,6 +24,11 @@ const Post = ({ match }) => {
         </Link>
         <PostItem post={post} showActions={false} />
         <CommentForm postId={post._id} />
+        <div className="comments">
+          {post.comments.map(comment => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+          ))}
+        </div>
       </Fragment>
     )
 }
